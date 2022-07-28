7ee2d2062a2ba9fad9eb731a6594934f77ed1f44
aseelalqasem <73478181+aseelalqasem@users.noreply.github.com>
2022-07-28 15:21:27 +0300

    Add files via upload

---
+++ nineteam/nineteam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nineteam
+//
+//  Created by Assel ALQasem on 29/12/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Ahad, say Hi to Abrar please ")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nineteam/nineteam/nineteamApp.swift
@@ -0,0 +1,17 @@
+//
+//  nineteamApp.swift
+//  nineteam
+//
+//  Created by Assel ALQasem on 29/12/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct nineteamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
